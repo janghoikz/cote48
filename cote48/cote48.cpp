017104082ae751f9ff75b1826c630763b394d04b
ohne <ghdlr785@gmail.com>
2022-08-09 17:30:54 +0900

    초기 커밋.

---
+++ cote48/cote48.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int count = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (count < 0)
+        {
+            return false;
+        }
+        if (s[i] == '(')
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+    }
+    if (s[0] == '(' && s.back() == ')' && count == 0)
+    {
+        answer = true;
+    }
+    else
+    {
+        answer = false;
+    }
+    
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
